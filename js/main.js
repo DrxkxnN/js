2111234ddc78f95f25469d4005ae685b28dcb90a
Drxkxn <alekseykuchin2004@gmail.com>
2023-09-07 16:20:38 +0300

    обновленно

--- js/main.js
+++ js/main.js
@@ -1,2 +1,37 @@
 
-alert
+// alert('Я В ПАРИЖЕ');
+
+// let a = 'text';
+// let b = 345;
+// console.log('я в париже')
+// console.log(a);
+// console.log(b);
+// console.log(4567)
+
+
+// let x = 100
+// let y = 100
+
+// console.log(x*y)
+// console.log(x+y)
+// console.log(x-y)
+// console.log(x/y)
+// console.log('--------------')
+
+// alert('Я В ПАРИЖЕ' + a);
+// // let what = prompt('Что')
+// // Number(what);
+// // console.log(Number(what))
+// let sure = confirm('Уверен')
+// console.log(sure)
+
+
+let a = prompt('Укажите название города')
+let b = Number(prompt('в каком году образовался город'))
+
+let d = Number(prompt('Какое каличество населения в этом городе'))
+
+let year_city = 2023-b
+
+alert('Городу ' + a + ' исполнилось ' + year_city + ' лет с момента его образования. Население - ' +d+ ' человек.')
+
