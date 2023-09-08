e7ba91e023352f8af8ddfdd61c2178263ed15d5c
Drxkxn <alekseykuchin2004@gmail.com>
2023-09-08 10:43:25 +0300

    new1

--- sm3/js/main.js
+++ sm3/js/main.js
@@ -2,10 +2,6 @@
 let a = Number(prompt('Укажите стоймость товара'))
 let b = Number(prompt('Введите количество своих Money'))
 
-if(a==b){
-    alert('Покупка совершена')
-}
-
 let x = a - b
 let c = b - a
 
